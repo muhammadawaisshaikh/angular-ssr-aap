b8e1d47c6180d978533a44c9a568cca0904f7570
Muhammad Awais <ashaikh@csquareonline.com>
2019-05-29 15:52:50 +0500

    some content changes

--- some-amazing-project/src/app/app.component.ts
+++ some-amazing-project/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'some-amazing-project';
+  title = 'Angular Universal & Server-side rendering Deep-Dive123';
 }
